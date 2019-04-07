3028914014d85c01274f25ae135b72235574ff1e
Stephanie Glover <stephanie.glover@ufl.edu>
2019-04-07 01:07:25 -0700

    Add a new instance to the subclass

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ class Desserts {
 }
 
 //create a subclass and extend the parent class
-class IceCream extends Desserts
+class IceCream extends Desserts {
   constructor (type, calories, flavor, scoops) {
   //invoke our parent constructor function passing in any parameters
 super(type, calories);
@@ -19,7 +19,9 @@ this.flavor = scoops;
 //adding method into the class ClassName
 
 includeSpoon () {
-  
+console.log('Here is your spoon!');
 }
 
 }
+
+const vanillaIceCream = new IceCream (Ice Cream, 340, Vanilla, 3) 
